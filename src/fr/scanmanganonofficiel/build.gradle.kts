8a111d62900a9139d01ab7294815aa99c3dd8518
Yumesion <Yumesion@users.noreply.github.com>
2026-09-09 09:45:32 +0000

    Add ScanManga (non officiel) extension

---
+++ src/fr/scanmanganonofficiel/build.gradle.kts
@@ -0,0 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "ScanManga (non officiel)"
+    versionCode = 1
+    contentWarning = ContentWarning.MIXED
+    libVersion = "1.4"
+
+    source {
+        baseUrl = "https://m.scan-manga.com"
+        lang = "fr"
+    }
+}
